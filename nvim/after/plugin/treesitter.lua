f8895c9221619fc568c0f4d0a7f15967aa0f842e
Wandering Raven <igorfm13524@gmail.com>
2023-12-22 19:35:43 -0300

    simplifica configuração do treesitter

---
+++ nvim/after/plugin/treesitter.lua
@@ -0,0 +1,13 @@
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = {"lua", "html", "css", "javascript", "typescript"},
+    
+    sync_install = false,
+
+    -- Install missing parsers when entering buffer
+    auto_install = true,
+
+    highlight = {
+        enable = true,
+    },
+}
+
