1a0cfe4adc89353bf52fc2e869591833c1b8d7a6
Wandering Raven <igorfm13524@gmail.com>
2023-12-22 19:40:19 -0300

    Torna instalação dos formatadores automática

--- nvim/lua/wandering-raven/plugins.lua
+++ nvim/lua/wandering-raven/plugins.lua
@@ -1,48 +1,46 @@
 local plugins = {
-    -- Appearance
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-        dependencies = 'nvim-lua/plenary.nvim'
-    },
-    'shaunsingh/nord.nvim',
+	-- Appearance
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		dependencies = "nvim-lua/plenary.nvim",
+	},
+	"shaunsingh/nord.nvim",
 
-    -- Completers
-    'windwp/nvim-autopairs',
-    'windwp/nvim-ts-autotag',
-    {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v3.x',
-        dependencies = {
-            { 'williamboman/mason.nvim' },
-            { 'williamboman/mason-lspconfig.nvim' },
+	-- Completers
+	"windwp/nvim-autopairs",
+	"windwp/nvim-ts-autotag",
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v3.x",
+		dependencies = {
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-            { 'neovim/nvim-lspconfig' },
+			{ "neovim/nvim-lspconfig" },
 
-            { 'hrsh7th/nvim-cmp' },
-            { 'hrsh7th/cmp-nvim-lsp' },
-            { 'L3MON4D3/LuaSnip' },
-        }
-    },
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "L3MON4D3/LuaSnip" },
+		},
+	},
 
-    -- Formatters
-    {
-        "stevearc/conform.nvim",
-        config = function()
-            require("conform").setup()
-        end,
-    },
+	-- Formatters
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup()
+		end,
+	},
 
-    -- Navigation
-    'theprimeagen/harpoon',
-    'tpope/vim-fugitive',
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    { 'akinsho/toggleterm.nvim', version = "*", config = true },
+	-- Navigation
+	"ThePrimeagen/harpoon",
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.5",
+	},
+	{ "akinsho/toggleterm.nvim", version = "*", config = true },
 }
 
 require("lazy").setup(plugins, {})
-
