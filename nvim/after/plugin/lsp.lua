76f463fb7e3811ab245f2beb7c8df2258aa5e330
Igor Moraes <igorfm13524@gmail.com>
2024-04-07 16:00:59 -0300

    Adiciona TS Server ao Volar
    devido à mudanças no  LSP do Vue, agora é necessário apontar onde está o typescript.

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -12,57 +12,51 @@ require("mason-tool-installer").setup({
 		"prettier",
 		"stylua",
 		"black",
+		"emmet-language-server",
+	},
+	handlers = {
+		lsp_zero.default_setup,
 	},
 })
 require("mason-lspconfig").setup({
 	ensure_installed = {
 		"cssls",
-		"emmet_ls",
 		"html",
 		"lua_ls",
-		"rust_analyzer",
 		"tsserver",
 		"volar",
 		"pyright",
 	},
 	handlers = {
 		lsp_zero.default_setup,
+		volar = function()
+			require("lspconfig").volar.setup({})
+		end,
+		tsserver = function()
+			local vue_typescript_plugin = require("mason-registry")
+				.get_package("vue-language-server")
+				:get_install_path() .. "/node_modules/@vue/language-server" .. "/node_modules/@vue/typescript-plugin"
+
+			require("lspconfig").tsserver.setup({
+				init_options = {
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vue_typescript_plugin,
+							languages = { "javascript", "typescript", "vue" },
+						},
+					},
+				},
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"javascript.jsx",
+					"typescript",
+					"typescriptreact",
+					"typescript.tsx",
+					"vue",
+				},
+			})
+		end,
 	},
-})
-
-local cmp = require("cmp")
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp_zero.defaults.cmp_mappings({
-	["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-	["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-	["<CR>"] = cmp.mapping.confirm({ select = true }),
-	["<C-Space>"] = cmp.mapping.complete(),
-})
-vim.api.nvim_create_autocmd("LspAttach", {
-	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-	callback = function(ev)
-		-- Enable completion triggered by <c-x><c-o>
-		vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-		-- Buffer local mappings.
-		-- See `:help vim.lsp.*` for documentation on any of the below functions
-		local opts = { buffer = ev.buf }
-		vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-		vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-		vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-		vim.keymap.set("n", "<space>wl", function()
-			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-		end, opts)
-		vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-		vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-		vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
-		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-		vim.keymap.set("n", "<space>f", function()
-			vim.lsp.buf.format({ async = true })
-		end, opts)
-	end,
 })
--- nvim/lua/wandering-raven/plugins.lua
+++ nvim/lua/wandering-raven/plugins.lua
@@ -10,21 +10,17 @@ local plugins = {
     -- Completers
 	"windwp/nvim-autopairs",
 	"windwp/nvim-ts-autotag",
-	"WhoIsSethDaniel/mason-tool-installer.nvim",
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v3.x",
-		dependencies = {
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
 
-			{ "neovim/nvim-lspconfig" },
+    {'williamboman/mason.nvim'},
+    {"WhoIsSethDaniel/mason-tool-installer.nvim"},
+    {'williamboman/mason-lspconfig.nvim'},
 
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "L3MON4D3/LuaSnip" },
-		},
-	},
+    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {'L3MON4D3/LuaSnip'},
+	
 
    	-- Formatters
 	{
