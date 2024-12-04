f274c71aeda0739adbb2b83c9a0c25f83f952cb0
Igor Moraes <76961399+IgorFerreiraMoraes@users.noreply.github.com>
2024-12-04 15:18:29 -0300

    Node autostart
    O script apenas instalava o Node, toda vez que fosse usar era necessário apontar uma versão para o nvm

--- configuration scripts/install_programs.sh
+++ configuration scripts/install_programs.sh
@@ -39,9 +39,9 @@
 	
 	# Node && NPM
 	wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-	export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+	export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 	nvm install node
+	nvm alias default node
 	
 	# Godot
 	flatpak install flathub org.godotengine.Godot --noninteractive
