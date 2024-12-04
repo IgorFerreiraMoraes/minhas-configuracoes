e86c087347e0fc2999006132f241577a0bd593a6
Igor Moraes <76961399+IgorFerreiraMoraes@users.noreply.github.com>
2024-12-04 14:03:16 -0300

    Neovim: Adiciona plugin smear-cursor

---
+++ nvim/after/plugin/smear_cursor.lua
@@ -0,0 +1,5 @@
+require("smear_cursor").setup({
+	stiffness = 0.8,
+	trailing_stiffness = 0.5,
+	slowdown_exponent = 0.01,
+})
--- nvim/lua/wandering-raven/plugins.lua
+++ nvim/lua/wandering-raven/plugins.lua
@@ -1,35 +1,37 @@
 local plugins = {
-    -- Appearance
+	-- Appearance
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
 		dependencies = "nvim-lua/plenary.nvim",
 	},
 	"shaunsingh/nord.nvim",
+	{
+		"sphamba/smear-cursor.nvim",
+		opts = {},
+	},
 
-    -- Completers
+	-- Completers
 	"windwp/nvim-autopairs",
 	"windwp/nvim-ts-autotag",
 
-    {'williamboman/mason.nvim'},
-    {"WhoIsSethDaniel/mason-tool-installer.nvim"},
-    {'williamboman/mason-lspconfig.nvim'},
+	{ "williamboman/mason.nvim" },
+	{ "WhoIsSethDaniel/mason-tool-installer.nvim" },
+	{ "williamboman/mason-lspconfig.nvim" },
 
-    {'VonHeikemen/lsp-zero.nvim', branch = 'v3.x'},
-    {'neovim/nvim-lspconfig'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'hrsh7th/nvim-cmp'},
-    {'L3MON4D3/LuaSnip'},
-	
+	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
+	{ "neovim/nvim-lspconfig" },
+	{ "hrsh7th/cmp-nvim-lsp" },
+	{ "hrsh7th/nvim-cmp" },
+	{ "L3MON4D3/LuaSnip" },
 
-   	-- Formatters
+	-- Formatters
 	{
 		"stevearc/conform.nvim",
 		config = function()
 			require("conform").setup()
 		end,
 	},
-
 
 	-- Navigation
 	{
@@ -43,5 +45,4 @@ local plugins = {
 	},
 	{ "akinsho/toggleterm.nvim", version = "*", config = true },
 }
-
 require("lazy").setup(plugins, {})
