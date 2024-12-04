5d114305cecff33f85c0659261743962ebd3c3b9
Igor Moraes <76961399+IgorFerreiraMoraes@users.noreply.github.com>
2024-12-04 14:02:01 -0300

    Atualiza tsserver para ts_ls
    O pacote mudou de nome, no momento vai ficar comentado no setup porque nenhum dos dois está sendo reconhecido

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -23,7 +23,7 @@ require("mason-lspconfig").setup({
 		"cssls",
 		"html",
 		"lua_ls",
-		"ts_ls",
+		-- "ts_ls",
 		"volar",
 		"pyright",
 	},
@@ -32,12 +32,12 @@ require("mason-lspconfig").setup({
 		volar = function()
 			require("lspconfig").volar.setup({})
 		end,
-		tsserver = function()
+		ts_ls = function()
 			local vue_typescript_plugin = require("mason-registry")
 				.get_package("vue-language-server")
 				:get_install_path() .. "/node_modules/@vue/language-server" .. "/node_modules/@vue/typescript-plugin"
 
-			require("lspconfig").tsserver.setup({
+			require("lspconfig").ts_ls.setup({
 				init_options = {
 					plugins = {
 						{
