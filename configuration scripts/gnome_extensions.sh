fe454432c078d941378744ec49a51d0cd55ed104
Igor Moraes <igorfm13524@gmail.com>
2025-08-26 17:30:04 -0300

    Lista de extensões e arquivo de configuração

--- configuration scripts/gnome_extensions.sh
+++ configuration scripts/gnome_extensions.sh
@@ -1,25 +1,17 @@
-# Install Tweaks and Manager
-sudo dnf install gnome-tweaks jq -y
-flatpak install --noninteractive flathub com.mattjakeman.ExtensionManager
-
-# Define list of extensions to be installed
 extension_list=(
-    "forge@jmmaranan.com"
     "hidetopbar@mathieu.bidon.ca"
-    "user-theme@gnome-shell-extensions.gcampax.github.com"
-    "blur-my-shell@aunetx"
+    "paperwm@paperwm.github.com"
 )
 
-for extension in "${extension_list[@]}" do
-    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-query/?search=${extension}" | jq '.extensions[0] | .shell_version_map | map(.pk) | max')
+for extension in "${extension_list[@]}"; do
+    VERSION_TAG=$(curl -Lfs "https://extensions.gnome.org/extension-info/?uuid=${extension}" | jq '.shell_version_map | to_entries | map(.value.pk) | max')
     wget -O ${extension}.zip "https://extensions.gnome.org/download-extension/${extension}.shell-extension.zip?version_tag=$VERSION_TAG"
     gnome-extensions install --force ${EXTENSION_ID}.zip
     if ! gnome-extensions list | grep --quiet ${extension}; then
         busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s ${extension}
     fi
     gnome-extensions enable ${extension}
-    rm ${EXTENSION_ID}.zip
+    rm ${extension}.zip
 done
 
-gnome-extensions refresh
-
+dconf load /org/gnome/shell/extensions/ < ./resources/extensions.dconf
