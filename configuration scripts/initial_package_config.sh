0b254738d95e2592d383a9cad12a97d7bf2f5b57
Igor Moraes <igorfm13524@gmail.com>
2025-08-26 20:26:12 -0300

    Inverte a ordem de update e remoção
    Ao rodar o comando `update` após a remoção dos programas no mesmo script, eles continuaram instalados.

--- configuration scripts/initial_package_config.sh
+++ configuration scripts/initial_package_config.sh
@@ -3,6 +3,13 @@ flatpak remote-delete fedora
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update --appstream
 
+# Upgrade System
+sudo rpm-ostree update
+flatpak update -y
+sudo fwupdmgr refresh --force
+sudo fwupdmgr get-updates
+sudo fwupdmgr update
+
 # Remove Unwanted Programs
 sudo rpm-ostree override remove \
   firefox \
@@ -10,13 +17,6 @@ sudo rpm-ostree override remove \
   gnome-tour \
   gnome-user-docs
 sudo rpm-ostree cleanup -m
-
-# Upgrade System
-sudo rpm-ostree update
-flatpak update -y
-sudo fwupdmgr refresh --force
-sudo fwupdmgr get-updates
-sudo fwupdmgr update
 
 # Disable NetworkManager wait-online
 sudo systemctl disable NetworkManager-wait-online.service
