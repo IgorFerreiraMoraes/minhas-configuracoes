bb34cdf749206709b8344723eaddc6106fe5eebb
Igor Moraes <igorfm13524@gmail.com>
2025-09-03 12:59:25 -0300

    Corrige Race Condition com rpm-ostree
    Comandos `rpm-ostree` são não bloqueantes. Como usei `sudo rpm-ostree update` e logo depois `sudo rpm-ostree override remove`, tem duas transações acontecendo ao mesmo tempo.
    
    Usei a flag `--uninstall` para juntar ambos em um comando só.

--- configuration scripts/initial_package_config.sh
+++ configuration scripts/initial_package_config.sh
@@ -5,20 +5,16 @@ flatpak remote-delete fedora
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update --appstream
 
-# Upgrade System
-sudo rpm-ostree update
+# Remove Unwanted Programs and Upgrade System
+sudo rpm-ostree update \
+  --uninstall firefox \
+  --uninstall firefox-langpacks \
+  --uninstall gnome-tour \
+  --uninstall gnome-user-docs
 flatpak update -y
 sudo fwupdmgr refresh --force
 sudo fwupdmgr get-updates
 sudo fwupdmgr update
-
-# Remove Unwanted Programs
-sudo rpm-ostree override remove \
-  firefox \
-  firefox-langpacks \
-  gnome-tour \
-  gnome-user-docs
-sudo rpm-ostree cleanup -m
 
 # Disable NetworkManager wait-online
 sudo systemctl disable NetworkManager-wait-online.service
